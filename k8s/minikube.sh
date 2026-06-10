d1df41859a592e417169efcd926b5b9c71b50261
Robert Piasecki <piasecki.robert@gmail.com>
2026-06-10 11:56:47 +0200

    Add K8S support. Add ingress and TLS termination. Closes #7

--- k8s/minikube.sh
+++ k8s/minikube.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 # General
 minikube start
 minikube addons enable metrics-server
 minikube addons enable ingress
 
-kubectl create -f namespaces.yaml
+kubectl apply -f namespaces.yaml
 
 # Strimzi operator
 kns kafka
@@ -12,8 +14,16 @@ kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 # Wait for operator to fully start
 
 # Schema registry
-kubectl create -f schema-registry.yaml
+kubectl apply -f schema-registry.yaml
 
 # App
 kns spring-practice
-kubectl create -f springjpa.yaml
+kubectl apply -f springjpa.yaml
+
+# Ingress
+kns spring-practice
+kubectl -n spring-practice create secret tls ingress-tls --key tls/localhost.pem --cert tls/localhost.crt
+kubectl apply -f ingress.yaml
+
+# Minikube tunnel
+minikube tunnel
